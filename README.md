93f55b6f70ddb0f3b6aedef990eccecf6f6ffef9
lWarne <laurencewarne@gmail.com>
2019-07-10 18:26:11 +0100

    Remove planning.org from tracking. Add build status from CI to README.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Second Space
+[![Build Status](https://travis-ci.org/LaurenceWarne/second-space.svg?branch=master)](https://travis-ci.org/LaurenceWarne/second-space)
 
 An experimental space game.
--- core/src/test/java/laurencewarne/secondspace/server/system/WorldSerializationSystemTest.java
+++ core/src/test/java/laurencewarne/secondspace/server/system/WorldSerializationSystemTest.java
@@ -45,7 +45,7 @@ public class WorldSerializationSystemTest {
     private FileHandle worldSaveFile;
 
     private TestLogger logger = TestLoggerFactory.getTestLogger(
-	WorldSerializationSystem.class
+		WorldSerializationSystem.class
     );    
     private World world;
     private WorldSerializationManager wsm;
@@ -54,103 +54,103 @@ public class WorldSerializationSystemTest {
 
     @Before
     public void setUp() {
-	initWorld();
+		initWorld();
     }
 
     public void initWorld() {
-	MockitoAnnotations.initMocks(this);
-	WorldConfiguration setup = new WorldConfigurationBuilder()
-	    .with(
-		wsm = new WorldSerializationManager(),
-		sys = new WorldSerializationSystem(1f)
-	    )
-	    .build();
-	setup.register("worldSaveFile", worldSaveFile);
-	world = new World(setup);
-	wsm.setSerializer(new JsonArtemisSerializer(world));
-	doAnswer(new Answer<OutputStream>() {
-		@Override
-		public OutputStream answer(InvocationOnMock invocation) throws Throwable {
-		    return os = new ByteArrayOutputStream();
-		}
-	    }).when(worldSaveFile).write(false);
-	doAnswer(new Answer<InputStream>() {
-		@Override
-		public InputStream answer(InvocationOnMock invocation) throws Throwable {
-		    return new ByteArrayInputStream(os.toByteArray());
-		}
-	    }).when(worldSaveFile).read();	    
+		MockitoAnnotations.initMocks(this);
+		WorldConfiguration setup = new WorldConfigurationBuilder()
+			.with(
+				wsm = new WorldSerializationManager(),
+				sys = new WorldSerializationSystem(1f)
+			)
+			.build();
+		setup.register("worldSaveFile", worldSaveFile);
+		world = new World(setup);
+		wsm.setSerializer(new JsonArtemisSerializer(world));
+		doAnswer(new Answer<OutputStream>() {
+				@Override
+				public OutputStream answer(InvocationOnMock invocation) throws Throwable {
+					return os = new ByteArrayOutputStream();
+				}
+			}).when(worldSaveFile).write(false);
+		doAnswer(new Answer<InputStream>() {
+				@Override
+				public InputStream answer(InvocationOnMock invocation) throws Throwable {
+					return new ByteArrayInputStream(os.toByteArray());
+				}
+			}).when(worldSaveFile).read();	    
     }
 
     public void reloadWorld() {
-	initWorld();
-	wsm.load(worldSaveFile.read(), SaveFileFormat.class);
-	world.process();
+		initWorld();
+		wsm.load(worldSaveFile.read(), SaveFileFormat.class);
+		world.process();
     }
 
     @Test
     public void testCanSerializeSingleEntityWithSingleComponent() {
-	int id = world.create();
-	world.edit(id).create(ComponentX.class).text = "cool-component";
-	world.process();
-	// Just to make sure serialization has occurred
-	sys.serialize();
-	// create a new world, and load entities from file
-	reloadWorld();
+		int id = world.create();
+		world.edit(id).create(ComponentX.class).text = "cool-component";
+		world.process();
+		// Just to make sure serialization has occurred
+		sys.serialize();
+		// create a new world, and load entities from file
+		reloadWorld();
 
-	ComponentMapper<ComponentX> mX = world.getMapper(ComponentX.class);
-	assertTrue(mX.has(id));
-	assertEquals("cool-component", mX.get(id).text);
+		ComponentMapper<ComponentX> mX = world.getMapper(ComponentX.class);
+		assertTrue(mX.has(id));
+		assertEquals("cool-component", mX.get(id).text);
     }
 
     @Test
     public void testCanSerializeEmptyWorldAndGetEmptyWorld() {
-	world.process();
-	sys.serialize();
-	reloadWorld();
-	final IntBag allEntities = world
-	    .getAspectSubscriptionManager()
-	    .get(Aspect.all())
-	    .getEntities();
-	assertTrue(allEntities.isEmpty());
+		world.process();
+		sys.serialize();
+		reloadWorld();
+		final IntBag allEntities = world
+			.getAspectSubscriptionManager()
+			.get(Aspect.all())
+			.getEntities();
+		assertTrue(allEntities.isEmpty());
     }
 
     @Test
     public void testCanSerializeWorldWithMultipleEntitiesAndComponents() {
-	int id1 = world.create(), id2 = world.create();
-	world.edit(id1).create(ComponentX.class).text = "cool-component";
-	world.edit(id2).create(ComponentX.class).text = "another-cool-component";
-	world.edit(id1).create(ComponentY.class).f = 46f;
-	world.process();
-	sys.serialize();
-	reloadWorld();
-	ComponentMapper<ComponentX> mX = world.getMapper(ComponentX.class);
-	ComponentMapper<ComponentY> mY = world.getMapper(ComponentY.class);
-	assertTrue(mX.has(id1));
-	assertTrue(mX.has(id2));
-	assertEquals("cool-component", mX.get(id1).text);
-	assertEquals("another-cool-component", mX.get(id2).text);
-	assertEquals(46f, mY.get(id1).f, 0.0001f);
+		int id1 = world.create(), id2 = world.create();
+		world.edit(id1).create(ComponentX.class).text = "cool-component";
+		world.edit(id2).create(ComponentX.class).text = "another-cool-component";
+		world.edit(id1).create(ComponentY.class).f = 46f;
+		world.process();
+		sys.serialize();
+		reloadWorld();
+		ComponentMapper<ComponentX> mX = world.getMapper(ComponentX.class);
+		ComponentMapper<ComponentY> mY = world.getMapper(ComponentY.class);
+		assertTrue(mX.has(id1));
+		assertTrue(mX.has(id2));
+		assertEquals("cool-component", mX.get(id1).text);
+		assertEquals("another-cool-component", mX.get(id2).text);
+		assertEquals(46f, mY.get(id1).f, 0.0001f);
     }
 
     @Test
     public void testErrorLoggedWithInvalidFile() {
-	WorldConfiguration setup = new WorldConfigurationBuilder()
-	    .with(
-		wsm = new WorldSerializationManager(),
-		sys = new WorldSerializationSystem(1f)
-	    )
-	    .build();
-	setup.register("worldSaveFile", worldSaveFile);
-	when(worldSaveFile.write(anyBoolean()))
-	    .thenThrow(new GdxRuntimeException(""));
-	world = new World(setup);
-	sys.serialize();
-	List<LoggingEvent> events = logger.getAllLoggingEvents();
-	LoggingEvent lastLog = events.get(events.size() - 1);
-	assertThat(
-	    lastLog.getMessage(),
-	    matchesPattern(".*file.*not.*written.*")
-	);
+		WorldConfiguration setup = new WorldConfigurationBuilder()
+			.with(
+				wsm = new WorldSerializationManager(),
+				sys = new WorldSerializationSystem(1f)
+			)
+			.build();
+		setup.register("worldSaveFile", worldSaveFile);
+		when(worldSaveFile.write(anyBoolean()))
+			.thenThrow(new GdxRuntimeException(""));
+		world = new World(setup);
+		sys.serialize();
+		List<LoggingEvent> events = logger.getAllLoggingEvents();
+		LoggingEvent lastLog = events.get(events.size() - 1);
+		assertThat(
+			lastLog.getMessage(),
+			matchesPattern(".*file.*not.*written.*")
+		);
     }
 }
